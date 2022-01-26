7dda9e2054c8ba4b6f8bd528aa4374f7af7f2786
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-26 14:23:42 +0200

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formElement = document.querySelector('.feedback-form');
+const inputElement = document.querySelector('input[name=email]');
+const textAreaElement = document.querySelector('textarea[name=message]');
+
+// console.log(formElement);
+// console.log(inputElement);
+// console.log(textAreaElement);
+
+formElement.addEventListener('input', throttle(local, 500));
+formElement.addEventListener('submit', onSubmit);
+
+const savedData = localStorage.getItem('feedback-form-state');
+const parsedData = JSON.parse(savedData);
+
+if (parsedData) {
+  inputElement.value = parsedData.email;
+  textAreaElement.value = parsedData.message;
+}
+
+function local(event) {
+  const email = inputElement.value;
+  const message = textAreaElement.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  if (!message || !email) {
+    return alert('Заполни все поля!');
+  }
+  const objectData = {
+    message,
+    email,
+  };
+  console.log(objectData);
+  localStorage.removeItem('feedback-form-state');
+  formElement.reset();
+}
